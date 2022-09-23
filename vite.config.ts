8183ea3d9db392ddb141ccb9e6c1249da012aba7
jdelauney <delauney.jerome@gmail.com>
2022-09-23 13:38:11 +0200

    chore: fix vite hmr compatibility with windows

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import type { UserConfig } from "vitest/config";
+import type { UserConfig } from 'vitest/config';
 
 const test = {
   globals: true,
-  environment: "jsdom",
+  environment: 'jsdom',
   threads: false,
   watch: false,
-  reporter: 'verbose'
-} as UserConfig["test"];
+  reporter: 'verbose',
+} as UserConfig['test'];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: { port: 3000 },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+    },
+  },
   build: {
     minify: false,
   },
-  base: "./",
-  root: "",
+  base: './',
+  root: '',
   test,
 });
